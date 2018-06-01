4129e98c23e15cc68e6f2ee8c7ffcb5fd1458dd4
Teschel6 <elijah.tesch@mail.wlc.edu>
2018-05-31 22:23:12 -0500

    initial db migration & schema

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+#Models define db schema and constraints
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    #human readable name is first argument of field instance
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes= models.IntegerField(default=0)
