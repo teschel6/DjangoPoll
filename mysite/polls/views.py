6e92cb3986c6d687cfcbe74cb6b6838056983577
Teschel6 <elijah.tesch@mail.wlc.edu>
2018-06-05 16:56:37 -0500

    added views and forms

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,41 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
+from .models import Question
+
+
+#load latest 5 question itno index template
 def index(request):
-	return HttpResponse("Hello, World. You are now at the polls index.")
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html',context)
+
+
+#Error handling 404 Not found
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html',{'question':question})
+
+
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		# Redisplay the question voting form
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message':"You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		#Return  HttpResponseRedirect after successful dealing with POST data.
+		#prevents duplicate requests if USER hits the back button
+		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
